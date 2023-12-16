708c8f989ceb501731eb0a5c582f5f89adefae84
Jimmy Russel Kurniawan <80058599+jimmyrk@users.noreply.github.com>
2023-12-16 15:06:14 +0700

    coba

---
+++ variable.js
@@ -0,0 +1,16 @@
+const nama = "jim";
+nama = "jimjim";
+
+console.log(nama);
+
+// const student = {
+//   name: "jojo",
+//   class: 2,
+
+// }
+// student.faculty = "sainteks";
+// console.log(student);
+
+// const students = ['momo', 'lili', 'koko'];
+// students.push('lolo');
+// console.log(students);
